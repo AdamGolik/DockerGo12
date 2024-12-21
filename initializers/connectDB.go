3b72f3964c4d6b903e018dda0908b2917b6df6a4
AdamGolik <156575468+AdamGolik@users.noreply.github.com>
2024-12-21 16:32:39 +0100

    Update connectDB.go

--- initializers/connectDB.go
+++ initializers/connectDB.go
@@ -2,8 +2,9 @@ package initializers
 
 import (
 	"awesomeProject12/models"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql" // SQL driver for raw database connection
@@ -14,23 +15,18 @@ var DB *gorm.DB
 
 // func that will let us allow to create or connect to the database
 func ConnectDB() {
-	//an var error
 	var err error
-	//all var from .env file for db
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	dbname := os.Getenv("DB_NAME")
+	// Konfiguracja DSN dla PostgreSQL
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" port=" + os.Getenv("DB_PORT") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" sslmode=disable"
 
-	//connect to db
-	dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
-	//open db
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if there is error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		// it will type out the error
-		panic("Failed to connect to database: " + err.Error())
+		log.Fatal("Failed to connect to database:", err)
 	}
 }
 
